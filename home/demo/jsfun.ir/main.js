bfe7b2b9222f27712c18d39f2410ded292a101fc
Shakiba Moshiri <hi@shakiba.it>
2019-07-15 20:19:57 +0430

    update contents

---
+++ home/demo/jsfun.ir/main.js
@@ -0,0 +1,38 @@
+import React, { Fragment, Component } from "react";
+import { render } from "react-dom";
+const log = console.log;
+
+const Prism = ( props ) => <pre><code className="language-javascript">{ props.children }</code></pre>;
+const Title = ( props ) => <h1 className="title">{ props.children }</h1>;
+const SubTitle = ( props ) => <h3 className="sub-title">{ props.children }</h3>;
+
+if(!( "WebSocket" in window )){
+    alert( "Your Browser Does not Support HTML5 WebSocket API" );
+    exit;
+}
+
+const ws = new WebSocket( "ws://localhost:3330" );
+ws.onopen = function(){
+    log( "jsfun.ir web socket was opened" );
+}
+ws.onclose = function(){
+    log( "jsfun.ir web socket was closed" );
+}
+ws.onmessage = function( payload ){
+    displayData( payload.data );
+}
+
+function displayData( data ){
+    const pre = document.getElementById( "jsfun.ir" );
+    const code = document.createElement( "code" );
+    code.textContent = data;
+    pre.appendChild( code );
+}
+
+const description = <Fragment>
+    <p>Here is the log of <a target="_blank" href="http://jsfun.ir">jsfun.ir</a> which is a node js microserice for managing the blog.</p>
+    <p>Do not close this page and meanwhile open <a target="_blank" href="http://jsfun.ir">jsfun.ir</a> in a separate window with your browser.</p>
+    <p>Navigate between pages (= links) and watch here at the same time.</p>
+    <h3 class="sub-title">jsfun.ir log on the server right now:</h3>
+</Fragment>; 
+render( description, document.getElementById( "description" ) );
