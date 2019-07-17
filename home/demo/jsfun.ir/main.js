1f41bead4a1be7e81870455f5845feada76a9d12
Shakiba Moshiri <hi@shakiba.it>
2019-07-17 20:06:43 +0430

    Update for having 3 web sockets

--- home/demo/jsfun.ir/main.js
+++ home/demo/jsfun.ir/main.js
@@ -11,28 +11,51 @@ if(!( "WebSocket" in window )){
     exit;
 }
 
-const ws = new WebSocket( "ws://localhost:3330" );
-ws.onopen = function(){
-    log( "jsfun.ir web socket was opened" );
-}
-ws.onclose = function(){
-    log( "jsfun.ir web socket was closed" );
-}
-ws.onmessage = function( payload ){
-    displayData( payload.data );
+const services = {
+    logs: {
+        address: "localhost",
+        port: 3350,
+        target: "service-logs"
+    },
+    build: {
+        address: "localhost",
+        port: 3351,
+        target: "service-build"
+    },
+    status: {
+        address: "localhost",
+        port: 3352,
+        target: "service-status"
+    },
+};
+
+function webSocketManager( service ){
+    const ws = new WebSocket( `ws://${ service.address }:${ service.port }` );
+    ws.onopen = function(){
+        log( `web socket was opened for ${ service.target }` );
+    }
+    ws.onclose = function(){
+        log( `web socket was closed for ${ service.target }` );
+    }
+    ws.onmessage = function( payload ){
+        displayData( payload.data, service.target );
+    }
 }
 
-function displayData( data ){
-    const pre = document.getElementById( "jsfun-ir" );
+function displayData( data, target ){
+    const pre = document.getElementById( target );
     const code = document.createElement( "code" );
     code.textContent = data;
     pre.appendChild( code );
 }
 
+webSocketManager( services.logs );
+webSocketManager( services.build );
+webSocketManager( services.status );
+
 const description = <Fragment>
-    <p>Here is the log of <a target="_blank" href="http://jsfun.ir">jsfun.ir</a> which is a node js microserice for managing the blog.</p>
-    <p>Do not close this page and meanwhile open <a target="_blank" href="http://jsfun.ir">jsfun.ir</a> in a separate window with your browser.</p>
+    <p>Here is the log of <a target="_blank" href="http://rtsm.ir">rtsm.ir</a> which is a node js microserice for managing this dmoe.</p>
+    <p>Do not close this page and meanwhile open <a target="_blank" href="http://rtsm.ir">rtsm.ir</a> in a separate window with your browser.</p>
     <p>Navigate between pages (= links) and watch here at the same time.</p>
-    <h3 class="sub-title">jsfun.ir log on the server right now:</h3>
 </Fragment>; 
 render( description, document.getElementById( "description" ) );
