ad7c52756e96fabf2aa6d09e697689167aef8ac6
Shakiba Moshiri <hi@shakiba.it>
2019-07-27 09:39:26 +0430

    Fix typo

--- home/demo/rtsm.ir/main.js
+++ home/demo/rtsm.ir/main.js
@@ -54,7 +54,7 @@ webSocketManager( services.build );
 // webSocketManager( services.status );
 
 const description = <Fragment>
-    <p>Here is the log of <a target="_blank" href="http://rtsm.ir">rtsm.ir</a> which is a node js microserice for managing this dmoe.</p>
+    <p>Here is the log of <a target="_blank" href="http://rtsm.ir">rtsm.ir</a> which is a node js microservice for managing this dmoe.</p>
     <p>Do not close this page and meanwhile open <a target="_blank" href="http://rtsm.ir">rtsm.ir</a> in a separate window with your browser.</p>
     <p>Navigate between pages (= links) and watch here at the same time.</p>
 </Fragment>; 
